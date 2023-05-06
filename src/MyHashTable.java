f2ac7eccc55f4cd22031d3460c62b323b0dbe9f7
jonathan221483 <captainberkut@gmail.com>
2023-05-06 08:48:38 +0600

    constructor and initial setups

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,4 +1,7 @@
 public class MyHashTable<K,V> {
     private class HashNode<K,V>{}
-
+    private HashNode<K,V>[] chainArray;
+    private int chain_number = 11;
+    private int size;
+    public MyHashTable(){}
 }
