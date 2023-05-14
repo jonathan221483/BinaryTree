9185a0a85b753deb8068e80ead9993c0d73bb25c
jonathan221483 <captainberkut@gmail.com>
2023-05-15 02:56:54 +0600

    generator bug fixed

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,10 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         MyHashTable<Book, String> hashTable = new MyHashTable<>();
-        private static class Generator{
-            String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            Random random = new Random();
+        Generator gen = new Generator();
 
-            public String NameGen() {
-                String text = "";
-                int length = random.nextInt(5) + 10;
-                for (int i = 0; i < length; i++) {
-                    text+=letters.charAt(random.nextInt(letters.length()));
-                }
-                return text.toString();
-            }
 
-        }
+
+
     }
 }
