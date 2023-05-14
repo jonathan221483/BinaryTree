704f4f729a7cbace245bae4bffd9f7fe2c92025d
jonathan221483 <captainberkut@gmail.com>
2023-05-15 02:47:06 +0600

    Testing names generator

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,39 @@
+import java.util.Objects;
 public class Book {
+    private String name;
+    private int id;
+    private float cost;
 
+    public Book(String name, int group, float gpa){
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = hash * 5 + id;
+        hash = hash * 7 + (int) cost;
+
+        char[] letters = name.toCharArray();
+        int a = 0;
+        for (char i: letters) a = i + (31 * a);
+
+        hash = hash * 13 + a;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && Float.compare(book.cost, cost) == 0 && Objects.equals(name, book.name);
+    }
+
+    @Override
+    public String toString(){
+        return this.name + ' ' + this.id;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         MyHashTable<Book, String> hashTable = new MyHashTable<>();
+        private static class Generator{
+            String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Random random = new Random();
+
+            public String NameGen() {
+                String text = "";
+                int length = random.nextInt(5) + 10;
+                for (int i = 0; i < length; i++) {
+                    text+=letters.charAt(random.nextInt(letters.length()));
+                }
+                return text.toString();
+            }
+
+        }
     }
 }
