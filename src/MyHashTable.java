d015a047f93a364cf23f10ec8ad5bb1d065f3c42
jonathan221483 <captainberkut@gmail.com>
2023-05-14 17:17:13 +0600

    corrections

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,8 +1,10 @@
 public class MyHashTable<K,V> {
-    private class HashNode<K,V>{}
     private HashNode<K,V>[] chainArray;
     private int chain_number = 11;
-    private int size;
-    public MyHashTable(){}
+    private int size = 0;
+    public MyHashTable(){
+        this.chainArray = new HashNode[this.chain_number];
+    }
+
 
 }
