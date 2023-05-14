a0ed4223521a2b80dcd9329459347eff539fcb17
jonathan221483 <captainberkut@gmail.com>
2023-05-15 04:04:27 +0600

    final commit: no bugs, no warnings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         MyHashTable<Book, String> hashTable = new MyHashTable<>();
