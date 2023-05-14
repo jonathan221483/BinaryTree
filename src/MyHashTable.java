6e9734137ce71d9b8076a8053ae4eb46b84df83a
jonathan221483 <captainberkut@gmail.com>
2023-05-14 17:18:53 +0600

    constructor for HashTable and hash method

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -5,6 +5,12 @@ public class MyHashTable<K,V> {
     public MyHashTable(){
         this.chainArray = new HashNode[this.chain_number];
     }
-
+    public MyHashTable(int chain_number){
+        this.chainArray = new HashNode[chain_number];
+        this.chain_number = chain_number;
+    }
+    public int hash(K key){
+        return Math.abs(key.hashCode() % this.chain_number);
+    }
 
 }
